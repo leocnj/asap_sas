802156c4dcd95895fa8b7bfb36be3bce90b243f5
Chen, Lei <LChen@ETS.ORG>
2016-02-04 22:12:47 -0500

    tested bow

--- asap_data.R
+++ asap_data.R
@@ -1,6 +1,5 @@
 #
 # asap_data.R
-#
 # process ASRP_SAS data stored at /Data
 #
 library(dplyr)
@@ -13,26 +12,45 @@ pub1 <- read.csv('Data/public_leaderboard_rel_2.tsv',
                  sep = "\t", stringsAsFactors = FALSE)
 
 pub2 <- read.csv('Data/public_leaderboard_solution.csv', stringsAsFactors = FALSE)
-
 pub <- merge(pub1, pub2[c("id", "essay_score")], by.x="Id", by.y="id")
 
-# focus on set 1 
-set1_train <- train %>%
-  filter(EssaySet==1) %>%
-  select(c(Id,Score1,EssayText))
-write.csv(set1_train, "set1_train.csv", row.names = F)
+oneset <- function(id){
+  trainFile <- paste('asap/train', id, '.csv', sep='')
+  testFile <- paste('asap/test', id, '.csv', sep='')
+  show(trainFile)
 
-set1_test <- pub %>%
-  filter(EssaySet==1) %>%
-  select(c(Id,essay_score,EssayText)) %>%
-  rename(Score1=essay_score)
-write.csv(set1_test, "set1_test.csv", row.names = F)
-
-
-
-
-
-
-
-
+  set_train <- train %>%
+    filter(EssaySet==id) %>%
+    select(c(Score1,EssayText)) %>%
+    rename(label=Score1, text=EssayText)
+  write.csv(set_train, trainFile, row.names = F)
   
+  set_test <- pub %>%
+    filter(EssaySet==id) %>%
+    select(c(essay_score,EssayText)) %>%
+    rename(label=essay_score,text=EssayText)
+  write.csv(set_test, testFile, row.names = F)
+  
+  if(max(set_train$label) != max(set_test$label)){
+    print("WRONG")
+  }
+  
+}
+
+# lapply(1:10, oneset)
+
+# test BOW baseline performance.
+# dplyr and the Metrics packages are so neat to work together.
+bow <- read.csv('Data/bag_of_words_benchmark.csv')
+colnames(bow) <- c("id", "pred")
+
+bow_all <- merge(bow, pub[c("Id", "EssaySet", "essay_score")], by.x = "id",
+                 by.y = "Id")
+
+require(Metrics)
+kappas <- bow_all %>%
+          group_by(EssaySet) %>%
+          summarize(kp=ScoreQuadraticWeightedKappa(essay_score, pred))
+
+kp_bow <-MeanQuadraticWeightedKappa(kappas$kp)
+show(kp_bow)
